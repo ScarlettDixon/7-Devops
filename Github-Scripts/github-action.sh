e862c9176f345cd312b165266cb9b730eb4a4aa2
Scarlett Dixon <scarlett.dixon@protonmail.com>
2024-01-29 17:53:18 +0000

    Ansible Workflow Update attempt for effective vagrant install

--- Github-Scripts/github-action.sh
+++ Github-Scripts/github-action.sh
@@ -8,10 +8,10 @@ function AS01(){
     wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg ;
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list ;
     sudo apt update && sudo apt install vagrant ;
-	echo "${PWD}"
-	ls -alh ;
 	#vagrant init ;
 	cd "${PWD}/NAS-Server-Ansible-Docker/testing/scripts/"
+	echo "${PWD}"
+	ls -alh ;
     bash setup.sh ;
 }
 
